968095c6747178ac0657a98d98abea1e9c3bb0e8
hoaibao9x <hongochoaibao@gmail.com>
2024-05-24 16:09:42 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,8 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}><a href="https://sotaymmo.com">🤚🏻
-VIP </a> by <a href="https://twitter.com/jotzilla">{config.author}</a></div>
+      <div className={styles.copyright}><a href="https://sotaymmo.com">SotayMMO </a> by <a href="https://twitter.com/jotzilla">{config.author}</a></div>
 
       <div className={styles.settings}>
         {hasMounted && (
